3079a2a87e9fdeb14d3a23c5d0c14b6b75e09710
karamakil <k.akil@everteam-gs.com>
2021-05-03 02:09:32 +0300

    first commit

---
+++ TaxiOperator/Controllers/DataController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TaxiOperator.BLL.DAL;
+using TaxiOperator.BLL.Manager;
+
+namespace TaxiOperator.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetRandom()
+        {
+            try
+            {
+                return Ok(CustomerManager.GetRandom());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpPost("SetVip")]
+        public IActionResult SetVip(int id)
+        {
+            try
+            {
+                CustomerManager.SetVip(id);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        [HttpGet("GetCabDriverShifts")]
+        public IActionResult GetCabDriverShifts()
+        {
+            try
+            {
+                return Ok(CabDriverManager.List());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+
+    }
+}
---
+++ TaxiOperator/Services/TokenService.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using TaxiOperator.BLL.DAL;
+using TaxiOperator.BLL.Interface;
+using TaxiOperator.BLL.Models;
+
+namespace TaxiOperator.Services
+{
+    public class TokenService : ITokenService
+    {
+        private readonly SymmetricSecurityKey key;
+
+        public TokenService(IConfiguration config)
+        {
+            key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
+
+        public string CreateToken(User user)
+        {
+            var claims = new List<Claim>(){
+                new Claim(JwtRegisteredClaimNames.NameId, user.Id.ToString()),
+                new Claim(JwtRegisteredClaimNames.UniqueName, user.UserName),
+            };
+
+
+            //var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
+
+            var cred = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = cred
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
